8b6655dbbf1566994265244356c17f381a702f4c
juju4 <juju4@users.noreply.github.com>
2016-10-29 22:00:31 -0400

    typo

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -12,5 +12,5 @@ fi
 [ ! -d $rolesdir/geerlingguy.java ] && git clone https://github.com/geerlingguy/ansible-role-java.git $rolesdir/geerlingguy.java
 [ ! -d $rolesdir/geerlingguy.elasticsearch ] && git clone https://github.com/geerlingguy/ansible-role-elasticsearch.git $rolesdir/geerlingguy.elasticsearch
 [ ! -d $rolesdir/geerlingguy.postgresql ] && git clone https://github.com/geerlingguy/ansible-role-postgresql.git $rolesdir/geerlingguy.postgresql
-[ ! -d $rolesdir/juju4.gift ] && git clone https://github.com/juju4/ansible-role-gift.git $rolesdir/juju4.gift
+[ ! -d $rolesdir/juju4.gift ] && git clone https://github.com/juju4/ansible-gift.git $rolesdir/juju4.gift
 
