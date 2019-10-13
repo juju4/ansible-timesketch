1c3574b6d26aa59e02c737790f0571bae29c375d
juju4 <juju4@users.noreply.github.com>
2019-10-13 07:11:14 -0400

    use forked geerlingguy.postgresql

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -12,7 +12,7 @@ fi
 [ ! -d $rolesdir/geerlingguy.java ] && git clone https://github.com/geerlingguy/ansible-role-java.git $rolesdir/geerlingguy.java
 #[ ! -d $rolesdir/geerlingguy.elasticsearch ] && git clone https://github.com/geerlingguy/ansible-role-elasticsearch.git $rolesdir/geerlingguy.elasticsearch
 [ ! -d $rolesdir/geerlingguy.elasticsearch ] && git clone https://github.com/juju4/ansible-role-elasticsearch.git $rolesdir/geerlingguy.elasticsearch
-[ ! -d $rolesdir/geerlingguy.postgresql ] && git clone https://github.com/geerlingguy/ansible-role-postgresql.git $rolesdir/geerlingguy.postgresql
+[ ! -d $rolesdir/geerlingguy.postgresql ] && git clone https://github.com/juju4/ansible-role-postgresql.git $rolesdir/geerlingguy.postgresql
 [ ! -d $rolesdir/juju4.gift ] && git clone https://github.com/juju4/ansible-gift.git $rolesdir/juju4.gift
 ## galaxy naming: kitchen fails to transfer symlink folder
 #[ ! -e $rolesdir/juju4.timesketch ] && ln -s ansible-timesketch $rolesdir/juju4.timesketch
