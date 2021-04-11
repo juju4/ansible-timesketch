dc2651a013b5d6000bf020b7103cd8e3d20f9b71
juju4 <juju4@users.noreply.github.com>
2021-04-11 12:53:40 -0400

    test/default-docker: update serverspec

--- test/integration/default-docker/serverspec/timesketch_spec.rb
+++ test/integration/default-docker/serverspec/timesketch_spec.rb
@@ -3,12 +3,12 @@ require 'serverspec'
 # Required by serverspec
 set :backend, :exec
 
-describe process("tsctl") do
+describe process("docker-proxy") do
 
-  its(:user) { should eq "_timesketch" }
+  its(:user) { should eq "root" }
 
-  it "is listening on port 5000" do
-    expect(port(5000)).to be_listening
+  it "is listening on port 80" do
+    expect(port(80)).to be_listening
   end
 
 end
