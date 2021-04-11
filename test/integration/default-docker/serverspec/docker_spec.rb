4bd9afc71bdf845d0d22f0c3f6b57a9cecb02acb
juju4 <juju4@users.noreply.github.com>
2021-04-11 13:26:30 -0400

    test/default-docker: add some docker serverspec tests (2)

--- test/integration/default-docker/serverspec/docker_spec.rb
+++ test/integration/default-docker/serverspec/docker_spec.rb
@@ -3,13 +3,6 @@ require 'serverspec'
 # Required by serverspec
 set :backend, :exec
 
-describe file('/usr/bin/log2timeline.py') do
-  it { should be_file }
-  it { should be_executable }
-#  it { should be_executable.by('root') }
-  it { should be_executable.by('others') }
-end
-
 describe command('docker --version') do
   its(:stdout) { should match /Docker version .*, build / }
   its(:stderr) { should_not match /Error/i }
@@ -28,7 +21,7 @@ describe command('docker image ls') do
   its(:exit_status) { should eq 0 }
 end
 
-describe command("docker exec -it timesketch_postgres_1 psql -U timesketch -c '\l'") do
+describe command("docker exec -it timesketch_postgres_1 psql -U timesketch -c '\\l'") do
   its(:stdout) { should match /timesketch/ }
   its(:stderr) { should_not match /Error/i }
   its(:exit_status) { should eq 0 }
